06f15fba89532c87aeac17d8312e2e0e46177082
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-09-01 13:38:15 +0300

    Add project files.

---
+++ HW02_C++_OOP.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <time.h>
+
+const int MAX_SIZE = 128;
+const int TYPE_SIZE = 4;
+
+int main()
+{
+	srand(time(NULL));
+	using std::cout;
+	using std::cin;
+	using std::endl;
+
+	int arrCode[MAX_SIZE]{};
+	int sizeCode = 12;
+
+	cout << "Start!\n";
+
+	return 0;
+}
