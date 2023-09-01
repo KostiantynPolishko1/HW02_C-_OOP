95508b5767e9762e96c5b7fdd003c7072a331c33
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-09-01 15:57:44 +0300

    1 Created VARIANT1 to fill 12 fields of password by different type of symbols.

--- HW02_C++_OOP.cpp
+++ HW02_C++_OOP.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <time.h>
 
-const int MAX_SIZE = 128;
+const int MAX_SIZE = 12;
 const int TYPE_SIZE = 4;
+const int STEP_CODE = 4;
 
 int main()
 {
@@ -11,10 +12,50 @@ int main()
 	using std::cin;
 	using std::endl;
 
-	int arrCode[MAX_SIZE]{};
-	int sizeCode = 12;
+	int arrNum[MAX_SIZE]{0};
+	char arrPassword[MAX_SIZE]{};
+	bool isCode = false;
 
 	cout << "Start!\n";
+
+	for (short int n = 0; n < MAX_SIZE; n += STEP_CODE) //generation type symbol code
+	{
+		int arrTemp[TYPE_SIZE]{ 0 };
+
+		for (short int i = 0; i < TYPE_SIZE;) {
+
+			isCode = false;
+			int numRandType = rand() % TYPE_SIZE;
+
+			for (short int j = 0; j < i; j++) {
+
+				if (arrTemp[j] == numRandType) {
+					isCode = true;
+					break;
+				}
+			}
+
+			if (!isCode) {
+				arrTemp[i] = numRandType;
+				i++;
+			}
+		}
+
+		for (short int i = 0; i < TYPE_SIZE; i++)
+			arrNum[i + n] = arrTemp[i];
+	}
+
+	cout << "\nbefore\n";
+	for (const short int& element : arrNum)
+		cout << element<< " | ";
+
+	for (short int i = STEP_CODE; i < MAX_SIZE; i += STEP_CODE)//check neighbour items is equal
+		if (arrNum[i] == arrNum[i - 1])
+			std::swap(arrNum[i], arrNum[i+1]);
+
+	cout << "\nafter\n";
+	for (const short int& element : arrNum)
+		cout << element << " | ";
 
 	return 0;
 }
